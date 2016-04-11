fa133ff2b598f97c9c3917ceacfb07bb1c9018c8
WebFreak001 <janju007@web.de>
2016-04-11 19:56:47 +0200

    workspace-d supports the newest DCD version now

--- source/app.d
+++ source/app.d
@@ -50,41 +50,6 @@ bool dubInstall(string folder, string git, string[] output,
 	return true;
 }
 
-bool dubInstallDCD(string folder, string git, string[] output,
-	string[][] compilation = [["dub", "upgrade"], ["dub", "build", "--build=release"]])
-{
-	writeln("Cloning " ~ folder ~ " into ", tmp);
-	if (proc(["git", "clone", "-q", git, folder], tmp) != 0)
-	{
-		writeln("Error while cloning " ~ folder ~ ".");
-		return false;
-	}
-	string cwd = buildNormalizedPath(tmp, folder);
-	string tag = "v0.7.5";
-	if (tag.canFind(" "))
-	{
-		writeln("Invalid tag in git repository.");
-		return false;
-	}
-	writeln("Checking out ", tag);
-	if (proc(["git", "checkout", "-q", tag], cwd) != 0)
-	{
-		writeln("Error while checking out " ~ folder ~ ".");
-		return false;
-	}
-	writeln("Compiling...");
-	foreach (args; compilation)
-		if (proc(args, cwd) != 0)
-		{
-			writeln("Error while compiling " ~ folder ~ ".");
-			return false;
-		}
-	foreach (bin; output)
-		copy(buildNormalizedPath(cwd, bin), buildNormalizedPath("bin", bin.baseName));
-	writeln("Successfully compiled " ~ folder ~ "!");
-	return true;
-}
-
 int main(string[] args)
 {
 	if (!exists("bin"))
@@ -167,7 +132,7 @@ int main(string[] args)
 		if (!dubInstall("workspace-d",
 				"https://github.com/Pure-D/workspace-d.git", ["./workspace-d"]))
 			return 1;
-		if (dcd && !dubInstallDCD("DCD", "https://github.com/Hackerpilot/DCD.git",
+		if (dcd && !dubInstall("DCD", "https://github.com/Hackerpilot/DCD.git",
 				["./bin/dcd-client", "./bin/dcd-server"], [["git", "submodule",
 				"update", "-q", "--init", "--recursive"], ["make", "-j2"]]))
 			return 1;
